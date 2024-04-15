1db4be19bb6304fe8ece9b7f25ff117b6a5580a3
pigliwigli <ericmansilla33@gmail.com>
2024-04-15 18:21:34 +0200

    Login and register components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,28 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const  routes: Routes = [
     {
         path: 'main',
         component: MainComponent, 
     },
-    {
+    {//Other route that no exist
         path: '',
         redirectTo: '/main',
         pathMatch: 'full'
     },
     {
         path: 'productdetail/:id', component: ProductDetailComponent
+    },
+    {//Redirect to login
+        path: 'login',
+        component: LoginComponent,
+    },
+    {//Redirect to register
+        path: 'register',
+        component: RegisterComponent
     }
-
-    
 ];
