4d9ec5b2863ace208daef5d34f652db0bd3cbe5f
pigliwigli <ericmansilla33@gmail.com>
2024-04-23 16:59:18 +0200

    registerfuncional

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj: Login;
+  constructor(private http: HttpClient ) {
+    this.loginObj = new Login;
+  }
+
+  onLogin() {
+    this.http.post('http://172.17.131.10:3000/api/auth/', this.loginObj).subscribe((res: any)=>{
+      debugger;
+      if (res.result) {
+        alert("Login Success");
+      }else{
+        alert(res.message);
+      }
+    })
+  }
+}
+
+export class Login {
+
+  usuario: string;
+  nombre: string;
+  apellido1: string;
+  apellido2: string;
+  contrasena: string;
+
+  constructor() {
+    this.usuario = '';
+    this.nombre = '';
+    this.apellido1 = '';
+    this.apellido2 = '';
+    this.contrasena = '';
+  }
 
 }
---
+++ src/app/interfaces/users.ts
@@ -0,0 +1,10 @@
+export interface user {
+    usuario: string;
+    nombre: string;
+    apellido1: string;
+    apellido2: string;
+    contrasena: string;
+}
+export interface usersResults{
+    users: usersResults[]
+}
