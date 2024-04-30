687d4e2787079fad4f557326fe50f33bb566a3fb
oriolalmval <oriolalmval@campus.monlau.com>
2024-04-30 16:43:18 +0200

    PAGE WORKING

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { MainComponent } from './components/main/main.component';
-<<<<<<< HEAD
 import { ShopLocationComponent } from './components/shop-location/shop-location.component';
 import { ContactComponent } from './components/contact/contact.component';
-=======
-import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
->>>>>>> 74aca3f5f58ca9bf8d5419b64b857d92e5da583a
+import { LoginComponent } from './components/login/login.component';
+
 
 export const  routes: Routes = [
     {
@@ -23,20 +21,18 @@ export const  routes: Routes = [
     {
         path: 'productdetail/:id', component: ProductDetailComponent
     },
-<<<<<<< HEAD
     {
         path: 'shop', component: ShopLocationComponent
     },
     {
         path: 'contact', component: ContactComponent
-=======
-    {//Redirect to login
+    },
+    {
         path: 'login',
         component: LoginComponent,
     },
     {//Redirect to register
         path: 'register',
         component: RegisterComponent
->>>>>>> 74aca3f5f58ca9bf8d5419b64b857d92e5da583a
     }
 ];
